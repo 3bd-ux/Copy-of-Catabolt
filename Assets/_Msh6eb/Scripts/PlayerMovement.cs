2264f09a3c4261eef18fd69f5f7aae098f7da7c9
abed ebrahem <abed-1997@hotmail.com>
2024-10-26 20:57:02 +0200

    first commit

---
+++ Assets/_Msh6eb/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 1f;
+    [SerializeField] private Transform objectToMove;
+    [SerializeField] private Vector2 direction = new Vector2(1f, 0f);
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        if (objectToMove == null)
+        {
+            objectToMove = transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveObject(objectToMove);
+    }
+    void MoveObject(Transform obj)
+    {
+        Vector2 movement;
+        movement = direction.normalized * speed * Time.deltaTime;
+        obj.Translate(movement);
+    }
+}
